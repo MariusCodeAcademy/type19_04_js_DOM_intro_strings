425620a19580c83adf9982d7976d8d80eaaf2dd0
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-11-08 10:03:24 +0200

    dom

---
+++ js/dom.js
@@ -0,0 +1,37 @@
+'use strict';
+console.log('dom.js file was loaded');
+
+// gauti #intro el teksta
+// pakeisti #intro el teksta
+// darasyti kazka prie intro el tekxo
+
+// nusitaikyti ir gauti kintamaji kur rodo i ta el
+const pIntroEl = document.getElementById('intro');
+console.log('pIntroEl ===', pIntroEl);
+
+// gauti teksta is elemento
+let pText = pIntroEl.textContent;
+console.log('pText ===', pText);
+
+// pakeisti #intro el teksta
+// pIntroEl.textContent =
+//   'Sitas tekstas yra dinamiskas ir nesimato per vie page source';
+
+// pIntroEl.textContent = prompt('apie ka');
+
+// darasyti po intro tekstu " about DOM"
+
+pIntroEl.textContent = pText + ' about DOM';
+// pIntroEl.textContent = pIntroEl.textContent + ' about DOM';
+// pIntroEl.textContent += ' about DOM';
+
+// padidinti intro teksta i 50px
+pIntroEl.style.fontSize = '50px';
+pIntroEl.style.lineHeight = 1;
+
+// prideti(perrasyti) klase
+pIntroEl.className = 'dynamic';
+
+// pakeisti <li id="pirma">red</li> spalva
+// gauti nuoroda i sarasa <ul id="color-list"> su document.getElementById ir su document.querySelector();
+// gauti visus li su document.querySelectorAll();
