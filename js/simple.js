c0725779f113a12883c0e6d0708d50e5d2fac391
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-11-08 11:49:54 +0200

    simp

---
+++ js/simple.js
@@ -0,0 +1,6 @@
+'use strict';
+console.log('simple.js file was loaded');
+
+// pakeisti straipsnio title i 'HTML'
+
+// pakeisti teksta po juo ir 'Hypertext Markup Language, a standardized system for tagging text files to achieve font, colour, graphic, and hyperlink effects on World Wide Web pages.'
