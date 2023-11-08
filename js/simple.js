5c626710c1b2cc9280ba5dd77bc4591bb23e408c
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-11-08 11:54:45 +0200

    uzd

--- js/simple.js
+++ js/simple.js
@@ -1,6 +1,14 @@
 'use strict';
 console.log('simple.js file was loaded');
 
-// pakeisti straipsnio title i 'HTML'
+//1.  pakeisti straipsnio title i 'HTML'
 
-// pakeisti teksta po juo ir 'Hypertext Markup Language, a standardized system for tagging text files to achieve font, colour, graphic, and hyperlink effects on World Wide Web pages.'
+//2.  pakeisti teksta po juo ir 'Hypertext Markup Language, a standardized system for tagging text files to achieve font, colour, graphic, and hyperlink effects on World Wide Web pages.'
+
+// 3. uzdeti remeli ant <article id="art1">
+
+// 4. <li>blue</li> padaryti paryskintu (bold)
+
+// 5. <h1 class="main-title">Dom</h1> pakeisti teksta i 'Dinamiska antraste'
+
+// 6. <li>2004</li> - pakeisti teksta i '2024'
