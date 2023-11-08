68633f62d0a18c3683cd02ed7b30d5bf66052b62
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-11-08 10:45:34 +0200

    nth

--- js/innerHtml.js
+++ js/innerHtml.js
@@ -1,2 +1,19 @@
 'use strict';
 console.log('innerHtml.js file was loaded');
+
+// taikomes
+const outputEl = document.getElementById('output');
+const ulEl = document.querySelector('#color-list');
+// nusitaikyti i paskutini li
+const lastLi = document.querySelector('li:last-child ');
+
+console.log('lastLi ===', lastLi);
+console.log('outputEl ===', outputEl);
+outputEl.textContent = '<h2>I am sub title</h2>';
+outputEl.innerText = '<h2>I am sub title</h2>';
+outputEl.innerHTML = '<h2>I am sub title</h2>';
+
+//
+console.log('ulEl.innerHTML ===', ulEl.innerHTML);
+// isvalyti sarasa
+// ulEl.innerHTML = '';
