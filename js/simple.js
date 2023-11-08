581eabc3a0c8eee86a7e7a35117d9424b49c5b56
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-11-08 12:00:31 +0200

    uzd

--- js/simple.js
+++ js/simple.js
@@ -12,3 +12,13 @@ console.log('simple.js file was loaded');
 // 5. <h1 class="main-title">Dom</h1> pakeisti teksta i 'Dinamiska antraste'
 
 // 6. <li>2004</li> - pakeisti teksta i '2024'
+
+// 7. sukurti ir patalpinti paragrafa su tekstu (savo nuoziura tekstas) i <div id="aside-block"></div> (innerHtml)
+
+// 8. <li>green</li> padaryti kad sio el fono spalva butu tago tekstas
+
+// 9. <li>red</li> padaryti kad sio el text spalva butu tago tekstas
+
+// 10. sukurti kintamaji kurio tekstas butu tai kas yra irasyta i <p id="intro"> ir <p id="art__text">
+
+// 11. istrinti <h2 id="art__title"> teksta
